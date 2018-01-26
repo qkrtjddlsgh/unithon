4d496f8ac0ff08c5aca6ee59979a2a2ff0d61093
박성호 <glgl2131@naver.com>
2018-01-26 11:36:10 +0900

    경로 추가

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var create_diary = require('./routes/manage_diary/create_diary');
+var analyze_diary = require('./routes/manage_diary/analyze_diary');
 
 var app = express();
-
 
 // database connection
 var mongoose = require('mongoose');
@@ -36,6 +37,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/create_diary', create_diary);
+app.use('/analyze_diary', analyze_diary);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
