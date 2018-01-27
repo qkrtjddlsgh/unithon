5c21143ad07cf61214163f83b2d7ec2588977501
박성호 <glgl2131@naver.com>
2018-01-27 10:56:45 +0900

    post로 수정 테스트

--- routes/manage_book/search_book.js
+++ routes/manage_book/search_book.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var client_id = 'Uuri286TO_0SEQFRbb7t';
 var client_secret = 'BRnt6tr_Vt';
 
-router.get('/search/book', function (req, res) {
+router.post('/search/book', function (req, res) {
     // var api_url = 'https://openapi.naver.com/v1/search/book?query=' + encodeURI(req.query.query); // json 결과
     var api_url = 'https://openapi.naver.com/v1/search/book?query=' + encodeURI(req.query.query);
 
