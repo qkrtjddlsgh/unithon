39ef95f509a4cc3b844ce4399023a2007c749d80
박성호 <glgl2131@naver.com>
2018-01-27 11:36:36 +0900

    get 방식 재수정

--- routes/manage_book/search_book.js
+++ routes/manage_book/search_book.js
@@ -18,9 +18,7 @@ router.get('/search/book', function (req, res) {
     request.get(options, function (error, response, body) {
         if(!error && response.statusCode == 200) {
             res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
-
-            res.send(body);
-            res.end();
+            res.end(body);
         }
         else{
             res.status(response.statusCode).end();
