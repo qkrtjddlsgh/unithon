ea654e0b61608fb4893ba5fb6bf415e741207e16
박성호 <glgl2131@naver.com>
2018-01-27 22:48:12 +0900

    오타 수정

--- routes/manage_book/recommend_book.js
+++ routes/manage_book/recommend_book.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var review = require('../../models/Review');
+var review = require('../../models/Book');
 
 router.get('/', function(req, res){
 
