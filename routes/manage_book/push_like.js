3fa17da7ad9e521122dc217aae75bc6f439d190f
박성호 <glgl2131@naver.com>
2018-01-27 18:01:57 +0900

    like 수정

--- routes/manage_book/push_like.js
+++ routes/manage_book/push_like.js
@@ -8,7 +8,52 @@ router.put('/', function(req, res){
     var member_id = recv_data.id; // 클라이언트에서 넘겨받은 현재 접속 멤버 id
     var _id = recv_data._id; // review 고유 id
 
-    //이미 좋아요 누른 사람인지 체크
+    var chk = 1;
+
+    review.find({_id: _id}, function(err, result){
+        if(err){
+            console.error(err.message);
+        }
+        if(result.length == 0){
+            var res_data = new Object();
+            res_data.code = "8888";
+            res_data.message = "No review";
+
+            res.send(res_data);
+            res.end();
+        }
+        else{
+            for(var i=0; i<result[0].like.length; i++){
+                if(result[0].like.id == id){
+                    chk = 0;
+                }
+            }
+
+            if(chk){
+                review.update({_id: _id}, {$push: {like: member_id}}, function(err, result) {
+                    if(err){
+                        console.error(err.message);
+                    }
+                    var res_data = new Object();
+                    res_data.code = "9999";
+                    res_data.message = "pushed Like!";
+
+                    res.send(res_data);
+                    res.end();
+                });
+            }
+            else{
+                var res_data = new Object();
+                res_data.code = "8888";
+                res_data.message = "Already exist";
+
+                res.send(res_data);
+                res.end();
+            }
+        }
+    });
+
+    /*//이미 좋아요 누른 사람인지 체크
     review.find({like: { $contains : member_id }}, function(err, result){
         if(err){
             console.error(err.message);
@@ -33,7 +78,7 @@ router.put('/', function(req, res){
             res.send(res_data);
             res.end();
         });
-    });
+    });*/
 });
 
 module.exports = router;
