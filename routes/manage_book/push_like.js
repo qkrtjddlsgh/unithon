3a662e89b5900131f83c2bcee0c96abfc03b2ae7
박성호 <glgl2131@naver.com>
2018-01-27 18:12:12 +0900

    isbn, id로 수정

--- routes/manage_book/push_like.js
+++ routes/manage_book/push_like.js
@@ -6,11 +6,11 @@ router.put('/', function(req, res){
     var recv_data = req.body;
 
     var member_id = recv_data.id; // 클라이언트에서 넘겨받은 현재 접속 멤버 id
-    var _id = recv_data._id; // review 고유 id
+    var isbn = recv_data.isbn;
 
     var chk = 1;
 
-    review.find({_id: _id}, function(err, result){
+    review.find({id: member_id, isbn: isbn}, function(err, result){
         if(err){
             console.error(err.message);
         }
@@ -30,7 +30,7 @@ router.put('/', function(req, res){
             }
 
             if(chk){
-                review.update({_id: _id}, {$addToSet: {like: {"id": member_id}}}, function(err, result) {
+                review.update({id: member_id, isbn: isbn}, {$addToSet: {like: {"id": member_id}}}, function(err, result) {
                     if(err){
                         console.error(err.message);
                     }
@@ -52,33 +52,6 @@ router.put('/', function(req, res){
             }
         }
     });
-
-    /*//이미 좋아요 누른 사람인지 체크
-    review.find({like: { $contains : member_id }}, function(err, result){
-        if(err){
-            console.error(err.message);
-        }
-        if(result.length != 0){
-            var res_data = new Object();
-            res_data.code = "8888";
-            res.data.message = "already clicked Like!";
-            res_data.response = result;
-            res.send(res_data);
-            res.end();
-        }
-
-        review.update({_id: _id}, { $push: { like: member_id } }, function(err, result) {
-            if(err){
-                console.error(err.message);
-            }
-            var res_data = new Object();
-            res_data.code = "9999";
-            res_data.message = "pushed Like!";
-            res_data.response = result;
-            res.send(res_data);
-            res.end();
-        });
-    });*/
 });
 
 module.exports = router;
