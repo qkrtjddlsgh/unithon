f04141a23b41e999021d2c6e2ecc6d488606aead
박성호 <glgl2131@naver.com>
2018-01-27 21:15:05 +0900

    중복 수정

--- routes/manage_book/recommend_book.js
+++ routes/manage_book/recommend_book.js
@@ -15,9 +15,19 @@ router.post('/', function(req, res){
                 add_data.push(result[i].isbn);
             }
 
+            var sorted_arr = add_data.slice().sort();
+
+            var results = [];
+
+            for (var i = 0; i < sorted_arr.length - 1; i++) {
+                if (sorted_arr[i + 1] == sorted_arr[i]) {
+                    results.push(sorted_arr[i]);
+                }
+            }
+
             var res_data = new Object();
             res_data.code = "9999";
-            res_data.response = add_data;
+            res_data.response = results;
 
             res.send(res_data);
             res.end();
