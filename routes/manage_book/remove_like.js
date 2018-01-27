615b01998827aa5e5c74495c4b118ce28d320695
박성호 <glgl2131@naver.com>
2018-01-27 18:14:10 +0900

    canceled like 추가

---
+++ routes/manage_book/remove_like.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+var review = require('../../models/Review');
+
+router.put('/', function(req, res){
+    var recv_data = req.body;
+
+    var member_id = recv_data.id; // 클라이언트에서 넘겨받은 현재 접속 멤버 id
+    var isbn = recv_data.isbn;
+
+    var chk = 1;
+
+    review.find({id: member_id, isbn: isbn}, function(err, result){
+        if(err){
+            console.error(err.message);
+        }
+        if(result.length == 0){
+            var res_data = new Object();
+            res_data.code = "8888";
+            res_data.message = "No review";
+
+            res.send(res_data);
+            res.end();
+        }
+        else{
+
+            for(var i=0; i<result[0].like.length; i++){
+                if(result[0].like.id == member_id){
+                    chk = 0;
+                }
+            }
+
+            if(chk){
+                var res_data = new Object();
+                res_data.code = "8888";
+                res_data.message = "not exist";
+
+                res.send(res_data);
+                res.end();
+            }
+            else{
+                review.update({id: member_id, isbn: isbn}, {$pull: {like: {"id": member_id}}}, function(err, result) {
+                    if(err){
+                        console.error(err.message);
+                    }
+                    var res_data = new Object();
+                    res_data.code = "9999";
+                    res_data.message = "canceled like!";
+
+                    res.send(res_data);
+                    res.end();
+                });
+            }
+        }
+    });
+});
+
+module.exports = router;
